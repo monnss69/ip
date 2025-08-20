2d237e68a3a5670fc02bf09ea6b5e6d35a614ec2
haiminh <phminh2105@gmail.com>
2025-08-20 15:38:31 +0800

    Added Level-2 features

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mon User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Mon.java
+++ src/main/java/Mon.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mon {
+    private static ArrayList<String> tasks = new ArrayList<>();
+    
     public static void main(String[] args) {
         System.out.println("    Hello I'm Mon. What can I do for you?");
         while (true) {
@@ -8,9 +11,21 @@ public class Mon {
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else {
+                handleInput(input);
             }
-            System.out.println("    Mon: " + input);
         }
         System.out.println("    Mon: See you again!");
+    }
+
+    private static void handleInput(String input) {
+        if (input.equals("list")) {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+            }
+        } else {
+            System.out.println("    added: " + input);
+            tasks.add(input);
+        }
     }
 }
