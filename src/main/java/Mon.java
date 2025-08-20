03b9fe6e688390c75fb285c40ecd9663022fb637
haiminh <phminh2105@gmail.com>
2025-08-20 15:23:39 +0800

    Added Level-1 feature

--- src/main/java/Mon.java
+++ src/main/java/Mon.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
+
 public class Mon {
     public static void main(String[] args) {
-        System.out.println("""
-                        ____________________________________________________________
-
-                        Hello I'm Mon. What can I do for you?
-                        ____________________________________________________________""");
-
-        System.out.println("""
-                        ____________________________________________________________
-
-                        See you again!
-                        ____________________________________________________________""");
+        System.out.println("    Hello I'm Mon. What can I do for you?");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("    Mon: " + input);
+        }
+        System.out.println("    Mon: See you again!");
     }
 }
